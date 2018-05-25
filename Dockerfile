9e651837700d9cc61da25ed980c953529e2dd10c
Ricardo Amaro <mail@ricardoamaro.com>
2018-05-25 22:26:47 +0100

    updated and oauth module

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apt-get -y install git curl wget supervisor openssh-server locales \
   mysql-client mysql-server apache2 pwgen vim-tiny mc iproute2 python-setuptools \
   unison netcat net-tools memcached nano libapache2-mod-php php php-cli php-common \
   php-gd php-json php-mbstring php-xdebug php-mysql php-opcache php-curl \
-  php-readline php-xml php-memcached; \
+  php-readline php-xml php-memcached php-oauth; \
   apt-get clean; \
   apt-get autoclean; \
   apt-get -y autoremove
--- drupal8_local.sh
+++ drupal8_local.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -e
 
-mkdir -p local/web local/data
+# Note: $LOCAL can be something else for several sites running from the console
+# eg. LOCAL=site1 ./drupal8_local.sh
 
-# Create a random local ssh port for the container
+LOCAL=${LOCAL:-"local"}
+
+mkdir -p ${LOCAL}/web ${LOCAL}/data
+
+# Create a random ${LOCAL} ssh port for the container
 export WEBPORT="$(((RANDOM % 10000)+ 50000))"
 export DBPORT="$(((RANDOM % 10000)+ 51000))"
 
-echo "**** Starting Drupal using persistence on local/ folder ****"
+echo "**** Starting Drupal using persistence on ${LOCAL}/ folder ****"
 echo "     Using MYSQL PORT: ${DBPORT}"
 echo "     Using HTTP  URL : http://localhost:${WEBPORT}"
 
@@ -16,8 +21,8 @@ docker network create --subnet=10.8.8.0/16 drupalnet 2>/dev/null || true
 # Run the container
 docker run -it \
   --net drupalnet \
-  --volume=${PWD}/local/data:/var/lib/mysql \
-  --volume=${PWD}/local:/var/www/html \
+  --volume=${PWD}/${LOCAL}/data:/var/lib/mysql \
+  --volume=${PWD}/${LOCAL}:/var/www/html \
   -p ${WEBPORT}:80 \
   -p ${DBPORT}:3306 \
   ricardoamaro/drupal8
