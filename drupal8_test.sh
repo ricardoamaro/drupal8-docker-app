1b694afbd745744835afb2673947c29c4f087d29
jptillman <jptillman@gmail.com>
2019-04-15 09:27:50 -0400

    Update drupal8_test.sh
    Fix bash/winshell variable syntax issue in echo statements as well

--- drupal8_test.sh
+++ drupal8_test.sh
@@ -5,8 +5,8 @@ export WEBPORT="$(((RANDOM % 10000)+ 50000))"
 export DBPORT="$(((RANDOM % 10000)+ 51000))"
 
 echo "**** Starting Drupal Trial Test Run with ephemeral data ****"
-echo "     Using MYSQL PORT: ${DBPORT}"
-echo "     Using HTTP  URL : http://localhost:${WEBPORT}"
+echo "     Using MYSQL PORT: %DBPORT%"
+echo "     Using HTTP  URL : http://localhost:%WEBPORT%"
 
 # Create a network
 docker network create --subnet=10.8.8.0/16 drupalnet 2>/dev/null || true
